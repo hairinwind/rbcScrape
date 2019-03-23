af5b00c354c67065c92e85f6bab89e1ea43c08ff
AEROINFO\yao.dong <yao.dong@aeroinfo.com>
2019-03-22 20:41:23 -0700

    check command argument

--- rbcScrape.py
+++ rbcScrape.py
@@ -32,7 +32,7 @@ def run():
 
 
 if __name__=="__main__":
-	runItNow = sys.argv[1] if len(sys.argv) >= 1 else None
+	runItNow = sys.argv[1] if len(sys.argv) >= 2 else None
 	if runItNow and runItNow.upper() == 'NOW':
 		run()
 	else :
